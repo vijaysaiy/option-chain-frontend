4d28162a71172810c20dc6b0c2ca9d907114559d
Naveen Jerubandi <naveen.jerubandi@tv2z.com>
2022-07-17 12:13:14 +0530

    changed nse website access

--- src/helpers/actions/apiCalls.js
+++ src/helpers/actions/apiCalls.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 export const Nifty_api = async () => {
+  axios.defaults.baseURL = 'https://www.nseindia.com';
+  axios.defaults.headers.post['Content-Type'] ='application/json;charset=utf-8';
+  axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
   // const res = await axios.get(`${process.env.REACT_APP_API_BASE_URL}/api/nse/nifty`);
   const res = await axios.get(
     "https://www.nseindia.com/api/option-chain-indices?symbol=NIFTY"
